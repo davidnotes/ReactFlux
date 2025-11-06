516b9baf31f0ad08784a73d8685de14f65115566
david <gu.jinxin@xinheyun.com>
2025-11-06 18:20:03 +0800

    feat: sync code and print version

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@ import useVersionCheck from "./hooks/useVersionCheck"
 import { settingsState } from "./store/settingsState"
 import { GITHUB_REPO_PATH } from "./utils/constants"
 import hideSpinner from "./utils/loading"
+// 获取 Package.json 版本号
+const packageJson = require("./package.json")
+const version = packageJson.version
 
 const localMap = {
   "de-DE": deDE,
@@ -29,6 +32,7 @@ const localMap = {
 const getLocale = (language) => localMap[language] || enUS
 
 const App = () => {
+  console.log(`current version：${version}`);
   useLanguage()
   useTheme()
   useFeedIconsSync()
